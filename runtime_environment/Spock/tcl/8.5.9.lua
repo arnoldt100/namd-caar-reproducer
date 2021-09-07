0a74ba4322127f73903d2f818fc97720e464838c
arnoldt <arnoldt@ornl.gov>
2021-09-07 13:51:37 -0400

    Initial tracking of file "8.5.9.lua".

---
+++ runtime_environment/Spock/tcl/8.5.9.lua
@@ -0,0 +1,26 @@
+-- -*- lua -*-
+
+help(
+[[
+This module loads tcl 8.5.9  
+]])
+
+local machine_name = "Spock"
+local software_name = "tcl"
+local tcl_version = "8.5.9"
+local ncp_top_level = os.getenv("NCP_TOP_LEVEL")
+local parent_directory = pathJoin(ncp_top_level,"sw",machine_name,software_name,tcl_version)
+
+local tcl_installation_directory = parent_directory
+local tcl_lib_dir = pathJoin(tcl_installation_directory,"/lib")
+local tcl_bin_dir = pathJoin(tcl_installation_directory,"/bin")
+local tcl_include_dir = pathJoin(tcl_installation_directory,"/include")
+local tcl_manpath_dir = pathJoin(tcl_installation_directory,"/man")
+
+-- -- No modifcations needed below this line
+setenv("TCL_DIR",parent_directory)
+prepend_path('MANPATH',tcl_manpath_dir)
+prepend_path('PATH',tcl_bin_dir)
+prepend_path('C_INCLUDE_PATH', tcl_include_dir)
+prepend_path('CPLUS_INCLUDE_PATH', tcl_include_dir)
+prepend_path('LD_LIBRARY_PATH',tcl_lib_dir)
