9a278d908836a579c060f28acd410709a7b2d113
arnoldt <arnoldt@ornl.gov>
2021-09-09 15:01:45 -0400

    Fixed an syntax error.

--- bin/download_tcl.sh
+++ bin/download_tcl.sh
@@ -1,18 +1,23 @@
 #! /usr/bin/env bash
 
+#-----------------------------------------------------
+# Set the tarball to download from sourceforge.      -
+#                                                    -
+#-----------------------------------------------------
+declare -r tarball="tcl8.5.9-src.tar.gz"
 
 #-----------------------------------------------------
 # The URL of the TCL source.                         -
 #                                                    -
 #-----------------------------------------------------
-declare -r source_to_download="https://sourceforge.net/projects/tcl/files/Tcl/8.5.9/tcl8.5.9-src.tar.gz"
+declare -r source_to_download="https://sourceforge.net/projects/tcl/files/Tcl/8.5.9/${tarball}"
 
 #-----------------------------------------------------
 # The destination folder of the downloaded TCL       -
 # source.                                            -
 #                                                    -
 #-----------------------------------------------------
-declare -r destination="${NCP_TOP_LEVEL}/sw/sources"
+declare -r destination="${NCP_TOP_LEVEL}/sw/sources/${tarball}"
 
 # ----------------------------------------------------
 # 
@@ -20,6 +25,9 @@ declare -r destination="${NCP_TOP_LEVEL}/sw/sources"
 # 
 # ----------------------------------------------------
 
-wget -L ${source_to_download} -o ${destination}
-
+cd ${NCP_TOP_LEVEL}
+wget -L ${source_to_download} -O ${destination}
+declare -r dir1=$(dirname ${destination})
+cd ${dir1}
+tar xzf ${tarball}
 
