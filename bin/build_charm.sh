c5b7d943753a7a8e520af36f6dc6eb1963a23a46
arnoldt <arnoldt@ornl.gov>
2021-10-13 11:55:40 -0400

    Intial tracking of file "netlrts-linux-x86_64-smp-gcc.lua".

--- bin/build_charm.sh
+++ bin/build_charm.sh
@@ -17,6 +17,84 @@
 
 #-----------------------------------------------------
 # Function:                                          -
+#    error_exit                                      -
+#                                                    -
+# Synopsis:                                          -
+#   An error handling function.                      -
+#   The prints the error and then forces the script  -
+#   to exit with a status 1.                         -
+#                                                    -
+# Positional parameters:                             -
+#   ${1} A string containing a descriptive error     -
+#        message                                     -
+#                                                    -
+#-----------------------------------------------------
+function error_exit () {
+    echo
+    echo "${SCRIPT_NAME}: ${1:-"Unknown Error"}" 1>&2
+    echo
+    exit 1
+}
+
+#-----------------------------------------------------
+# Function:                                          -
+#    error_no_exit                                   -
+#                                                    -
+# Synopsis:                                          -
+#   An error handling function.                      -
+#   Prints an error message.                         -
+#                                                    -
+# Positional parameters:                             -
+#   ${1} A string containing a descriptive error     -
+#        message                                     -
+#                                                    -
+#-----------------------------------------------------
+function error_no_exit {
+    echo "${SCRIPT_NAME}: ${1:-"Unknown Error"}" 1>&2
+    return
+}
+
+#-----------------------------------------------------
+# Function:                                          -
+#    usage                                           -
+#                                                    -
+# Synopsis:                                          -
+#   Prints the usage message.                        -
+#                                                    -
+# Positional parameters:                             -
+#   None                                             -
+#                                                    -
+#-----------------------------------------------------
+function usage {
+    local -r column_width=50
+    let separator_line_with=2*column_width+1
+    local -r help_frmt1="%-${column_width}s %-${column_width}s\n"
+    printf "\n"
+    printf "Usage:\n"
+    printf "\tbuild_charm++.sh [ -h | --help ] --target-machine <name of machine> --target-build <name of build>\n\n"
+    printf "${help_frmt1}" "option" "description"
+    for ((ip=0; ip < ${separator_line_with}; ip++));do
+        printf "%s" "-"
+    done
+    printf "\n"
+    printf "${help_frmt1}" "-h | --help" "Prints the help message"
+    printf "\n"
+    printf "${help_frmt1}" "--target-machine <name of machine>" "The machine to build NAMD on."
+    printf "${help_frmt1}" "--target-build" "The build target."
+    printf "\n"
+    printf "\n"
+    printf "${help_frmt1}" "Available target machine: " "Spock"
+    printf "${help_frmt1}" "Spock available target builds:" "mpi-linux-x86_64:smp:gnu"
+    printf "${help_frmt1}" "" "ofi-linux-x86_64:smp:gnu"
+    printf "${help_frmt1}" "" "netlrts-linux-x86_64:smp:gnu"
+    printf "\n"
+    printf "${help_frmt1}" "Available target machine: " "Summit"
+    printf "${help_frmt1}" "Summit available target builds:" "multicore-linux-ppc64le:gnu"
+    printf "\n"
+}
+
+#-----------------------------------------------------
+# Function:                                          -
 #    declare_global_variables                        -
 #                                                    -
 # Synopsis:                                          -
@@ -29,6 +107,27 @@
 #                                                    -
 #-----------------------------------------------------
 function declare_global_variables () {
+
+    #-----------------------------------------------------
+    # This variable stores the charm++ target machine.
+    # The target machine is the name of machine to build
+    # charm++ on.
+    #
+    #-----------------------------------------------------
+    declare -g ncp_target_machine="NOT_DEFINED"
+
+    #-----------------------------------------------------
+    # This variable stores the charm++ build target 
+    # configuration.
+    # 
+    #-----------------------------------------------------
+    declare -g ncp_target_build="NOT_DEFINED"
+
+    #-----------------------------------------------------
+    # Stores the the program options.
+    # 
+    #-----------------------------------------------------
+    declare -g OPTS=""
 
     #-----------------------------------------------------
     # Define this bash script name. This global variable
@@ -121,7 +220,7 @@ function check_script_prerequisites () {
 
 #-----------------------------------------------------
 # Function:                                          -
-#    build_ofi_linux_x86_64_gcc_smp_gfortran         -
+#    Spock_build_ofi_linux_x86_64_gcc_smp_gfortran   -
 #                                                    -
 # Synopsis:                                          -
 #   Builds charmm++ with the OFI transport layer.    -
@@ -132,7 +231,7 @@ function check_script_prerequisites () {
 # Positional parameters:                             -
 #                                                    -
 #-----------------------------------------------------
-function build_ofi_linux_x86_64_gcc_smp_gfortran() {
+function Spock_build_ofi_linux_x86_64_gcc_smp_gfortran() {
     #-----------------------------------------------------
     # The target of the build.
     # 
@@ -185,6 +284,129 @@ function build_ofi_linux_x86_64_gcc_smp_gfortran() {
     return
 }
 
+
+#-----------------------------------------------------
+# Function:                                          -
+#    Spock_build_netlrts_linux_x86_64_gcc_smp_gfortran -
+#                                                    -
+# Synopsis:                                          -
+#   Builds charmm++ with the netlrts transport       -
+#   layer.Debug symbols are included.                -
+#   This build is for the GNU programming            -
+#   environment.                                     -
+#                                                    -
+#                                                    -
+# Positional parameters:                             -
+#                                                    -
+#-----------------------------------------------------
+function Spock_build_netlrts_linux_x86_64_gcc_smp_gfortran () {
+    echo "Executing function Spock_build_netlrts_linux_x86_64_gcc_smp_gfortran"
+    #-----------------------------------------------------
+    # The target of the build.
+    # 
+    #-----------------------------------------------------
+    local -r target='charm++'
+
+    #-----------------------------------------------------
+    # Define the charm++ arch. This variable selects 
+    # the network transport layer to build. 
+    #-----------------------------------------------------
+    local -r charmarch='netlrts-linux-x86_64'
+
+    #-----------------------------------------------------
+    # Define  the options to the charm++ build command.
+    #-----------------------------------------------------
+    local -r options=" gcc smp -g -j4"
+
+    #-----------------------------------------------------
+    # Change to the charm++ source directory, and then
+    # run the charm++ build command. 
+    #-----------------------------------------------------
+    change_dir "${CHARM_SOURCE_DIRECTORY}"
+
+    echo "The charm++ build command: ./build ${target} ${charmarch} ${options}"
+
+    ./buildold ${target} ${charmarch} ${options}
+    if [ $? -ne 0 ];then
+       local -r build_message="Error! The script ${SCRIPT_NAME} failed to build charm++."
+       local -ir my_build_exit_status=${EXIT_STATUS["failed_build_command"]}
+       echo "${build_message}"
+       exit ${my_buildexit_status}
+    fi
+
+    change_dir "${SCRIPT_LAUNCH_DIR}"
+    return
+}
+
+#-------------------------------------------------------
+# Function:                                            -
+#   validate_command_line                              -
+#                                                      -
+# Synopsis:                                            -
+#   Validates this script command line.                -
+#   Once the command line arguments are validated,     -
+#   they are stored in the global variable OPTS.       -
+#   The user command getopt to validate                -
+#   the command line options.                          -
+#   Please read the user man pages for getopt:         -
+#   http://man7.org/linux/man-pages/man1/getopt.1.html -
+#                                                      -
+# Positional parameters:                               -  
+#   ${@} This script command line arguments.           -
+#                                                      -
+#-------------------------------------------------------
+function validate_command_line {
+    local -r short_options_without_args="h,"
+    local -r long_options_without_args="help,"
+    local -r long_options_with_args="target-machine:,target-build:,"
+    local -r long_options="${long_options_without_args}${long_options_with_args}"
+    local -r short_options="${short_options_without_args}${short_options_with_args}"
+    OPTS=$(getopt --options ${short_options} --long ${long_options} --name "${SCRIPT_NAME}" -- "$@")
+    if [ $? != 0 ]; then
+         usage
+         error_exit "The getopt command failed ... exiting"
+    fi
+}
+
+#-----------------------------------------------------
+# Function:                                          -
+#    parse_command_line                              -
+#                                                    -
+# Synopsis:                                          -
+#   Parses the command line arguments.               -
+#                                                    -
+# Positional parameters:                             -
+#   ${@} The command line arguments to this script.  -
+#-----------------------------------------------------
+function parse_command_line {
+    eval set -- $@
+    while true;do
+        case ${1} in
+            -h | --help ) 
+                usage
+                exit 0;;
+
+            --target-machine)
+                shift
+                ncp_target_machine=${1};;
+
+            --target-build)
+                shift
+                ncp_target_build=${1};;
+
+            -- ) 
+                shift
+                break;;
+
+            * ) 
+                echo "Internal parsing error!"
+                usage
+                exit 1;;
+        esac
+        shift
+    done
+}
+
 #-----------------------------------------------------
 # Below are the primary function calls to execute    -
 # the script.                                        -
@@ -197,5 +419,62 @@ check_script_prerequisites
 # Declaree global variables thar are used in this script.
 declare_global_variables
 
-# Call the function that builds charm++.
-build_ofi_linux_x86_64_gcc_smp_gfortran
+#-----------------------------------------------------
+#                                                    -
+# Start of main function of bash script.             -
+#                                                    -
+#-----------------------------------------------------
+function main () {
+
+    # Validate the command line options and arguments.
+    validate_command_line $@
+
+    # Parses the command line options.
+    parse_command_line ${OPTS}
+    if [ $? != 0 ]; then
+         error_exit 'The function parse_command_line failed ... exiting'
+    fi
+    
+    case ${ncp_target_machine} in
+        "Spock" )
+            case ${ncp_target_build} in
+
+                "ofi-linux-x86_64:smp:gnu" )
+                    Spock_build_ofi_linux_x86_64_gcc_smp_gfortran;;
+
+                "netlrts-linux-x86_64:smp:gnu" )
+                    Spock_build_netlrts_linux_x86_64_gcc_smp_gfortran;;
+
+                *)
+                    warn_unsupported_target "${ncp_target_build}" "${NCP_MACHINE_NAME}"
+                    exit 1;;
+
+            esac;;
+
+        "Summit" )
+            case ${ncp_target_build} in
+
+                "multicore-linux-ppc64le:gnu" )
+                    echo "Stud build for multicore-linux-ppc64le:gnu";;
+
+                *)
+                    warn_unsupported_target "${ncp_target_build}" "${NCP_MACHINE_NAME}"
+                    exit 1;;
+
+            esac;;
+
+         * ) 
+                echo "The machine '${NCP_MACHINE_NAME}' is not supported."
+                usage
+                exit 1;;
+    esac
+
+}
+
+#-----------------------------------------------------
+#                                                    -
+# End of main function of bash script.               -
+#                                                    -
+#-----------------------------------------------------
+
+main $@
