2225ccf6b2bffcdc7f2cadc0c97602d21af37eea
arnoldt <arnoldt@ornl.gov>
2021-10-21 16:40:40 -0400

    Initial tracking of file.

---
+++ bin/download_namd_amd.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+#-----------------------------------------------------
+# The github AMD NAMD ROCm repository.               -
+#                                                    -
+#-----------------------------------------------------
+declare -r repository="git@github.com:ROCmSoftwarePlatform/NAMD.git"
+
+# ----------------------------------------------------
+# The destination folder of the cloned NAMD.         -
+#                                                    -
+# ----------------------------------------------------
+declare -r destination="${NCP_TOP_LEVEL}/sw/sources/namd-amd"
+
+# ----------------------------------------------------
+# 
+#  No edits needed below this comment section
+# 
+# ----------------------------------------------------
+
+# We change to this package
+cd ${NCP_TOP_LEVEL}
+
+# Clone the repository from AMD.
+git clone ${repository} ${destination}
+
