f5854d0a20f91cfa6e91dd09f99efadb27fb7574
arnoldt <arnoldt@ornl.gov>
2021-11-15 12:40:13 -0500

    Updated the Lua module file to reflect latest programming environment of Spock.
    The latest modules are:
        PrgEnv-gnu/8.2.0
        cray-fftw/3.3.8.11

--- runtime_environment/Spock/Spock_core_runtime_environment.lua
+++ runtime_environment/Spock/Spock_core_runtime_environment.lua
@@ -5,9 +5,17 @@ help(
 This module sets critical environmental and path variables needed for building and running NAMD.
 ]])
 
+-- The Lua module version number for loading
+-- the Cray GNU programming environment. 
+local pe_gnu_version = "8.2.0"
+
+-- The Lua module version number for loading
+-- the Cray FFTW environment. 
+local cray_fftw_version = "3.3.8.11"
+
 -- A list of prerequisite modules.
-load("PrgEnv-gnu/8.0.0")
-load("cray-fftw/3.3.8.10")
+load("PrgEnv-gnu/" .. pe_gnu_version)
+load("cray-fftw/" .. cray_fftw_version)
 load("gnuplot/5.4-stable")
 
 -- Define the machine name.
