7d4ac9d3783bd1d40ee8e3ec1d755096bb334a9b
arnoldt <arnoldt@ornl.gov>
2022-06-27 10:30:26 -0400

    Initial tracking of files "Crusher-arnold-env.sh" and "Frontier-arnold-env.sh".

---
+++ Crusher-arnold-env.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+NCP_SCRATCH=/gpfs/alpine/scratch/arnoldt/stf006/scratch
+NCP_TOP_LEVEL=/ccs/home/arnoldt/namd-caar-reproducer
+NCP_PREFIX=/gpfs/alpine/scratch/arnoldt/stf006/sw
+NCP_RESULTS=/gpfs/alpine/scratch/arnoldt/stf006/result
---
+++ Frontier-arnold-env.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+# ----------------------------------------------------
+# The path to the namd-caar-reproducer 
+# scratch directory.
+# 
+# ----------------------------------------------------
+NCP_SCRATCH=${HOME}/scratch/namd
+
+# ----------------------------------------------------
+# The path to the namd-caar-reproducer package
+# top-level directory.
+# 
+# ----------------------------------------------------
+NCP_TOP_LEVEL=${HOME}/namd-caar-reproducer
+
+# ----------------------------------------------------
+# The path to the top-level directory where all 
+# dependent namd software is located. All namd 
+# dependent software is located under this
+# path.
+# 
+# ----------------------------------------------------
+NCP_PREFIX=${SCRATCH}/namd/Software
+
+# ----------------------------------------------------
+# The path to the top-level directory where all
+# namd benchmark results are located.
+# 
+# ----------------------------------------------------
+NCP_RESULTS=${NCP_SCRATCH}/namd/Results
