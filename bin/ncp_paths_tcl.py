d176b78101d7233b4b9d88f23278397c3953569b
arnoldt <arnoldt@ornl.gov>
2022-08-15 13:33:17 -0400

    Adding a new option to script.
    The new option is --machine-name

--- bin/ncp_paths_tcl.py
+++ bin/ncp_paths_tcl.py
@@ -4,16 +4,17 @@
 
 This module is intended to be executed as script. Typical usage is 
 
-    ncp_paths_tcl.py --software-name XXX --software-version YYY --NCP_PREFIX NCP_PE_KEY --path AAA
+    ncp_paths_tcl.py --software-name *softwarename* --software-version *softwareversion* --ncp-prefix *ncpprefix* --ncp-pe-key *ncppekey* --path *pathkey*
 
 where
 
-* XXX is the software name
-* YYY is the sotware version
-* NCP_PE_KEY is the programming environment key.
-* AAA is the key corresponding to the path to printed to stdout.
+* *softwarename* is the software name
+* *softwareversion* is the sotware version
+* *ncpprefix* is the top-level directory where all NAMD software dependencies are installed under
+* *ncppekey* is the corresponding key for the runtime programming environment
+* *pathkey* is the key corresponding to the path to printed to stdout.
 
-For a list of the available paths run the help option
+For a list of the available path keys run the help option
 
     ncp_paths_tcl.py --help
 """
@@ -26,30 +27,18 @@ import argparse # Needed for parsing command line arguments.
 from loggerutils.logger import create_logger_description
 from loggerutils.logger import create_logger
 
-## @fn _create_path_description( )
-## @brief Returns a string whose contents are the --path option description.
-## 
-## @return A string
-def _create_path_description():
-    frmt_header = "{0:20s} {1:40.40s}\n"
+def __create_path_description():
+    frmt_header = "{0:20s} {1:50.50s}\n"
     frmt_items = frmt_header
     header1 =  frmt_header.format("Option Values", "Path Returned" )  
     header1_len = len(header1)
-    log_option_desc = "The installation path to be returned. The options values are the following:\n\n"
+    log_option_desc = "The installation path to be returned. The permitted options values are the following:\n\n"
     log_option_desc += header1
     log_option_desc += "-"*header1_len  + "\n"
     log_option_desc += frmt_items.format("prefix", "The top-level installation directory for TCL.\n")  
     return log_option_desc
 
-## @fn parse_arguments( )
-## @brief Parses the command line arguments.
-##
-## @details Parses the command line arguments and
-## returns A namespace.
-##
-## @return A namespace. The namespace contains attributes
-##         that are the command line arguments.
-def parse_arguments():
+def __parse_arguments():
 
     import logging
 
@@ -72,6 +61,12 @@ def parse_arguments():
     # Adding mandatory argument group.
     mandatory_args_group = my_parser.add_argument_group(title="Mandatory Arguments")
 
+    mandatory_args_group.add_argument("--machine-name",
+        help="The name of the machine on which the sotware will be installed.",
+        required=True,
+        type=str,
+        metavar='<machine name>')
+
     mandatory_args_group.add_argument("--software-name",
         help="The name of the software package.",
         required=True,
@@ -84,20 +79,20 @@ def parse_arguments():
         type=str,
         metavar='YYY')
 
-    mandatory_args_group.add_argument("--NCP_PREFIX",
+    mandatory_args_group.add_argument("--ncp-prefix",
                            help="The top-level directory where all NAMD dependent software is installed under.",
                            required=True,
                            type=str,
                            metavar='NCP_PREFIX')
 
-    mandatory_args_group.add_argument("--NCP_PE_KEY",
+    mandatory_args_group.add_argument("--ncp-pe-key",
                            help="The key for setting the programming environment.",
                            required=True,
                            type=str,
                            metavar='NCP_PE_KEY')
                            
     mandatory_args_group.add_argument("--path",
-                                      help=_create_path_description(),
+                                      help=__create_path_description(),
                                       required=True,
                                       type=str,
                                       choices=[ _PATH_OPTIONS['prefix'][0] ],
@@ -106,23 +101,21 @@ def parse_arguments():
 
     return my_args 
 
-## @fn _print_prefix_path ()
-## @brief Prints the installation path to stdout.
-##
-## @param[in] path_option_value The option value for --path
-## @param[in] ncp_pe_key The programming environmnet key
-## @param[in] software_name The name of the software
-## @param[in] software_version The software version
-## @param[in] ncp_prefix The top-level installation directory under which the package will installed
-
-def _print_path(path_option_value):
+def __print_path(path_option_value,
+                 ncp_prefix,
+                 machine_name,
+                 software_name,
+                 software_version,
+                 ncp_pe_key):
     import sys
     fp = _PATH_OPTIONS[path_option_value]
-    tmp_path = fp[1]()
+    tmp_path = fp[1](ncp_prefix,machine_name,software_name,software_version,ncp_pe_key)
     sys.stdout.write(tmp_path)
 
-def __prefix_path():
-    return "Dummy prefix path"
+def __prefix_path(ncp_prefix,machine_name,software_name,software_version,ncp_pe_key):
+    import os
+    tmp_path = os.path.join(ncp_prefix,machine_name,software_name,software_version,ncp_pe_key)
+    return tmp_path
     
 ## @var dict _PATH_OPTIONS
 ## @brief Stores the option  and a function reference
@@ -136,13 +129,18 @@ _PATH_OPTIONS = {"prefix" : [ "prefix", __prefix_path] }
 ## @fn main ()
 ## @brief The main function.
 def main():
-    args = parse_arguments()
+    args = __parse_arguments()
     logger = create_logger(log_id='Default',
                            log_level=args.log_level)
 
     logger.info("Start of main program")
 
-    _print_path(args.path)
+    __print_path(args.path,
+                 args.ncp_prefix,
+                 args.machine_name,
+                 args.software_name,
+                 args.software_version,
+                 args.ncp_pe_key)
 
     logger.info("End of main program")
 
